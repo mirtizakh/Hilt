ccbc0f9c43129a280c54b920ed02c5dddaff3b32
M.Irtiza Khursheed <irtizakh@gmail.com>
2022-03-28 02:15:45 +0500

    Addition - Scope example is added

--- app/src/main/java/com/android/hilt/MainActivity.kt
+++ app/src/main/java/com/android/hilt/MainActivity.kt
@@ -2,12 +2,29 @@ package com.android.hilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @AndroidEntryPoint
+    class MyFragment : Fragment() {
+        @Inject
+        lateinit var someClass: SomeClass
+    }
+
+    @Singleton
+    class SomeClass @Inject constructor() {
+        fun doSomething() {
+            println("Do something")
+
+        }
     }
 }
